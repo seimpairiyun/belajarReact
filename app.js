99c4b4548c6994fa34d9243fa37d97eee1bfb24f
seimpairiyun <m.afizha96@gmail.com>
2022-05-24 19:48:50 +0700

    first commit

---
+++ app.js
@@ -0,0 +1,20 @@
+function print(n) {
+  console.log(n ?? "");
+}
+
+let d;
+const a = "A";
+const b = 100_000;
+const c = d ?? "alfabet";
+
+print();
+print(a);
+print(b);
+print(c);
+
+let hargaMinyak = 20_000
+hargaMinyak -= 5_000
+
+let hargaTelur = 20
+
+print(false || true)
